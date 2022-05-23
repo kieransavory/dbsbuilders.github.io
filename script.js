fde64208a9dad4bb390c5434f5edc8eaedcb591d
kieransavory <96089380+kieransavory@users.noreply.github.com>
2022-05-23 23:09:28 +0100

    Added preliminary files to Github
    Preliminary files added to github for version control. Media queries to be carried out.

---
+++ script.js
@@ -0,0 +1,7 @@
+function facebookOpen() {
+    window.open("https://www.facebook.com/dbsbuilders.co.uk/");
+}
+      
+function instagramOpen() {
+    window.open("https://www.instagram.com/darren.savory/?hl=en-gb")
+}
